b57b2ce329b28c12b0d1f81819e457ab90a16ecf
pavel golovchan <pavel-golovchan@yandex.ru>
2020-12-01 18:15:04 +0200

    урок_1

---
+++ lesson_01/spring-01/spring-01-exercise/src/main/java/ru/otus/spring/Main.java
@@ -0,0 +1,18 @@
+package ru.otus.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.otus.spring.domain.Person;
+import ru.otus.spring.service.PersonService;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // создайте здесь класс контекста
+        ClassPathXmlApplicationContext context =new ClassPathXmlApplicationContext("/spring-context.xml");
+        // Получите Person Service
+        PersonService service = context.getBean(PersonService.class);
+        // Получите Person "Ivan"
+        Person ivan = service.getByName("Ivan");
+        System.out.println("name: " + ivan.getName() + " age: " + ivan.getAge());
+    }
+}
